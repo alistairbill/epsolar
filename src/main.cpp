08890a0e92f40f43ea8d6b8c6b7c28e000961803
Alistair Bill <alistair.bill@gmail.com>
2017-11-19 22:01:53 +0000

    ESP-01 code with OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,147 @@
 #include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
 #include <ModbusMaster.h>
-#include <SoftwareSerial.h>
 
+#define EPSOLAR_DEVICE_ID 1
 
-// instantiate ModbusMaster object
+const char* ssid = "no";
+const char* password = "no";
+const char* mqtt_server = "rpi3.localdomain";
+const long interval = 120000;
+
+unsigned int solarVoltage, solarCurrent, batteryTemp, deviceTemp, loadVoltage, 
+    loadCurrent, batteryPercent, batteryVoltage, status;
+unsigned long solarPower, loadPower, batteryCurrent;
+unsigned long previousMillis = 0;
+uint8_t result;
+char buf[10];
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 ModbusMaster node;
-SoftwareSerial mySerial(10, 11); // RX, TX
+
+void reconnect()
+{
+  while (!client.connected()) {
+    if(client.connect("ESP8266Client")) {
+      client.subscribe("pond/load/status");
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  if (strcmp(topic, "pond/load/status") == 0) {
+    if ((char)payload[0] == '1' && status == 0) {
+      result = node.writeSingleRegister(0x903D, 0);
+      result = node.writeSingleCoil(0x0002, true);
+      status = 1;
+    } else if ((char)payload[0] == '0' && status == 1) {
+      result = node.writeSingleCoil(0x0002, false);
+      result = node.writeSingleRegister(0x903D, 3);
+      status = 0;
+    }
+  }
+}
 
 void setup()
 {
-  Serial.begin(9600);
-  mySerial.begin(115200);
-  node.begin(1, mySerial);
+  Serial.begin(115200);
+  delay(10);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  ArduinoOTA.begin();
+  node.begin(EPSOLAR_DEVICE_ID, Serial);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop()
 {
-  uint32_t result;
+  ArduinoOTA.handle();
 
-  result = node.readInputRegisters(0x3100, 18);
-  if (result == node.ku8MBSuccess) {
-    Serial.print("PV array voltage: ");
-    Serial.println(node.getResponseBuffer(0x00) / 100.0f);
-
-    Serial.print("PV array input current: ");
-    Serial.println(node.getResponseBuffer(0x01) / 100.0f);
-
-    Serial.print("Load voltage");
-    Serial.println(node.getResponseBuffer(0x0C) / 100.0f);
-
-    Serial.print("Load current");
-    Serial.println(node.getResponseBuffer(0x0D) / 100.0f);
-
-    Serial.print("Battery SOC");
-    Serial.println(node.getResponseBuffer(0x1A) / 100.0f);
-  } else {
-    Serial.println("error");
+  if (!client.connected()) {
+    reconnect();
   }
-  delay(5000);
+  client.loop();
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    result = node.readInputRegisters(0x3100, 18);
+    if (result == node.ku8MBSuccess) {
+      solarVoltage = node.getResponseBuffer(0x00);
+      itoa(solarVoltage, buf, 10);
+      client.publish("pond/solar/voltage", buf);
+
+      solarCurrent = node.getResponseBuffer(0x01);
+      itoa(solarCurrent, buf, 10);
+      client.publish("pond/solar/current", buf);
+
+      solarPower = node.getResponseBuffer(0x02) + (node.getResponseBuffer(0x03) << 16);
+      itoa(solarPower, buf, 10);
+      client.publish("pond/solar/power", buf);
+
+      loadVoltage = node.getResponseBuffer(0x0C);
+      itoa(loadVoltage, buf, 10);
+      client.publish("pond/load/voltage", buf);
+
+      loadCurrent = node.getResponseBuffer(0x0D);
+      itoa(loadCurrent, buf, 10);
+      client.publish("pond/load/current", buf);
+
+      loadPower = node.getResponseBuffer(0x0E) + (node.getResponseBuffer(0x0F) << 16);
+      itoa(loadPower, buf, 10);
+      client.publish("pond/load/power", buf);
+
+      batteryTemp = node.getResponseBuffer(0x10);
+      itoa(batteryTemp, buf, 10);
+      client.publish("pond/battery/temp", buf);
+
+      deviceTemp = node.getResponseBuffer(0x11);
+      itoa(deviceTemp, buf, 10);
+      client.publish("pond/temp", buf);
+    }
+    node.clearResponseBuffer();
+    delay(5);
+
+    result = node.readInputRegisters(0x311A, 1);
+    if (result == node.ku8MBSuccess) {
+      batteryPercent = node.getResponseBuffer(0x00);
+      itoa(batteryPercent, buf, 10);
+      client.publish("pond/battery/percent", buf);
+    }
+    node.clearResponseBuffer();
+    delay(5);
+
+    result = node.readInputRegisters(0x331A, 3);
+    if (result == node.ku8MBSuccess) {
+      batteryVoltage = node.getResponseBuffer(0x00);
+      itoa(batteryVoltage, buf, 10);
+      client.publish("pond/battery/voltage", buf);
+
+      batteryCurrent = node.getResponseBuffer(0x01) + (node.getResponseBuffer(0x02) << 16);
+      itoa(batteryCurrent, buf, 10);
+      client.publish("pond/battery/current", buf);
+    }
+    node.clearResponseBuffer();
+
+    result = node.readInputRegisters(0x3202, 1);
+    if (result == node.ku8MBSuccess) {
+      status = node.getResponseBuffer(0x00);
+      itoa(status, buf, 10);
+      client.publish("pond/load/status", buf);
+    }
+    node.clearResponseBuffer();
+  }
 }
