75321c8b33d9fec2046a36b4ad9319a36e980958
Alistair Bill <alistair.bill@gmail.com>
2017-11-29 16:53:23 +0000

    More attempts to get the load to switch on/off

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ const long interval = 120000;
 const long timeUpdateInterval = 3600000;
 
 unsigned int solarVoltage, solarCurrent, batteryTemp, deviceTemp, loadVoltage,
-    loadCurrent, batteryPercent, batteryVoltage, status;
+  loadCurrent, batteryPercent, batteryVoltage, status, day, monthYear;
 unsigned long solarPower, loadPower, batteryCurrent;
 unsigned long previousMillis = 0;
 uint8_t result;
@@ -42,12 +42,40 @@ void callback(char* topic, byte* payload, unsigned int length)
 {
   if (strcmp(topic, "pond/load/status") == 0) {
     if ((char)payload[0] == '1' && status == 0) {
-      result = node.writeSingleCoil(0x0001, true);
-      result = node.writeSingleCoil(0x0002, true);
+      // set manual mode
+      node.clearTransmitBuffer();
+      node.setTransmitBuffer(0, 0);
+      result = node.writeMultipleRegisters(0x903D, 1);
+      // set default off
+      node.clearTransmitBuffer();
+      node.setTransmitBuffer(0, 0);
+      result = node.writeMultipleRegisters(0x906A, 1);
+      // set load on
+      result = node.writeSingleCoil(0x0002, 1);
       status = 1;
+
     } else if ((char)payload[0] == '0' && status == 1) {
-      result = node.writeSingleCoil(0x0002, false);
-      result = node.writeSingleCoil(0x0001, false);
+      // set load off
+      result = node.writeSingleCoil(0x0002, 0);
+
+      // set automatic mode
+      node.clearTransmitBuffer();
+      node.setTransmitBuffer(0, 3);
+      result = node.writeMultipleRegisters(0x903D, 1);
+      // set 'use one time'
+      node.clearTransmitBuffer();
+      node.setTransmitBuffer(0, 0);
+      result = node.writeMultipleRegisters(0x9069, 1);
+
+      // set turn on/off time (11:00:37 - 13:00:37)
+      node.clearTransmitBuffer();
+      node.setTransmitBuffer(0, 37);
+      node.setTransmitBuffer(1, 0);
+      node.setTransmitBuffer(2, 11);
+      node.setTransmitBuffer(3, 37);
+      node.setTransmitBuffer(4, 0);
+      node.setTransmitBuffer(5, 13);
+      result = node.writeMultipleRegisters(0x9042, 6);
       status = 0;
     }
   }
@@ -82,15 +110,17 @@ void loop()
 
   if (currentMillis - previousMillis >= interval) {
     if (currentMillis - previousMillis >= timeUpdateInterval) {
-      unsigned int day = 1;
-      result = node.readInputRegisters(0x9014, 1);
+      result = node.readInputRegisters(0x9014, 2);
       if (result == node.ku8MBSuccess) {
-        day = node.getResponseBuffer(0x00);
+        day = (node.getResponseBuffer(0x00) >> 8);
+        monthYear = node.getResponseBuffer(0x01);
       }
       timeClient.update();
+      node.clearTransmitBuffer();
       node.setTransmitBuffer(0, timeClient.getSeconds() + (timeClient.getMinutes() << 8));
-      node.setTransmitBuffer(0, timeClient.getHours() + (day << 8));
-      result = node.writeMultipleRegisters(0x9013, 2);
+      node.setTransmitBuffer(1, timeClient.getHours() + (day << 8));
+      node.setTransmitBuffer(2, monthYear);
+      result = node.writeMultipleRegisters(0x9013, 3);
     }
 
     previousMillis = currentMillis;
