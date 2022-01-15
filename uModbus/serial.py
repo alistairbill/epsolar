6a0dd61501fc594d1499b15074a84705fef4f984
Alistair Bill <alistair.bill@gmail.com>
2022-01-15 00:36:02 +0000

    Improve Modbus driver

--- uModbus/serial.py
+++ uModbus/serial.py
@@ -13,98 +13,119 @@ import uasyncio as asyncio
 from uModbus import functions
 from micropython import const
 
-_READ_COILS = const(0x01)
-_READ_DISCRETE_INPUTS = const(0x02)
-_READ_HOLDING_REGISTERS = const(0x03)
-_READ_INPUT_REGISTER = const(0x04)
-_WRITE_SINGLE_COIL = const(0x05)
-_WRITE_SINGLE_REGISTER = const(0x06)
-_WRITE_MULTIPLE_COILS = const(0x0F)
-_WRITE_MULTIPLE_REGISTERS = const(0x10)
-
-_CRC_LENGTH = const(0x02)
+_CRC_LENGTH = const(2)
 _ERROR_BIAS = const(0x80)
-_RESPONSE_HDR_LENGTH = const(0x02)
-_ERROR_RESP_LEN = const(0x05)
+_ERROR_RESP_LEN = const(5)
+
+
+class IncompleteReadError(EOFError):
+    def __init__(self, partial, expected):
+        super().__init__(f'{len(partial)} bytes read on a total of {expected} expected bytes')
+        self.partial = partial
+        self.expected = expected
+
 
 class Serial:
-    def __init__(self, uart_id, baudrate):
+    def __init__(self, uart_id: int, baudrate: int):
         uart = UART(uart_id, baudrate=baudrate)
         self.uart = uart
         self.uart_reader = asyncio.StreamReader(uart)
         self.uart_writer = asyncio.StreamWriter(uart)
 
+
     @staticmethod
-    def _crc16(data: bytearray) -> bytes:
+    @micropython.viper
+    def _crc16(data) -> int:
         crc = 0xFFFF
-        for char in data:
-            crc ^= char
+        buf = ptr8(data)
+        n = int(len(data))
+        for i in range(n):
+            crc ^= buf[i]
             for _ in range(0, 8):
                 if (crc & 1) > 0:
                     crc = (crc >> 1) ^ 0xA001
                 else:
                     crc >>= 1
-        return struct.pack('<H', crc)
+        return crc
 
-    async def _send_receive(self, modbus_pdu: bytes, slave_addr: int, count: bool) -> bytearray:
+
+    async def _read_exactly(self, n: int):
+        buffer = bytearray()
+        i = n
+        while i:
+            res = await self.uart_reader.read(i)
+            if res is not None:
+                if not res:
+                    raise IncompleteReadError(buffer, n)
+                buffer += res
+                i -= len(res)
+        return buffer
+
+
+    async def _send_receive(self, modbus_pdu: bytes, slave_addr: int, length) -> bytearray:
         # flush rx buffer
         self.uart.read()
 
-        reader = self.uart_reader
         writer = self.uart_writer
 
         serial_pdu = bytearray()
         serial_pdu.append(slave_addr)
         serial_pdu.extend(modbus_pdu)
-        crc = self._crc16(serial_pdu)
+        crc = struct.pack("<H", self._crc16(serial_pdu))
         serial_pdu.extend(crc)
 
         writer.write(serial_pdu)
         await writer.drain()
 
-        header = await asyncio.wait_for_ms(reader.readexactly(_ERROR_RESP_LEN), 100)
-        if header[1] >= _ERROR_BIAS:
-            raise ValueError("slave returned exception code: {:d}".format(header[2]))
-        remainder = await reader.readexactly(_RESPONSE_HDR_LENGTH + 1 + header[2] + _CRC_LENGTH - _ERROR_RESP_LEN)
-        response = header + remainder
-        return self._validate_resp_hdr(response, slave_addr, count)
+        header_length, body_length = length
+        try:
+            response = await self._read_exactly(header_length + body_length + _CRC_LENGTH)
+            return self._validate_resp_hdr(response, slave_addr, length)
+        except IncompleteReadError as err:
+            if err.partial[1] >= _ERROR_BIAS:
+                raise ValueError(f"slave returned exception code: {err.partial[2]}") from err
+            return self._validate_resp_hdr(err.partial, slave_addr, length)
+
 
     @staticmethod
-    def _validate_resp_hdr(response: bytearray, slave_addr: int, count: bool) -> bytearray:
-        resp_crc = response[-_CRC_LENGTH:]
-        expected_crc = Serial._crc16(response[0:len(response) - _CRC_LENGTH])
-        if resp_crc[0] != expected_crc[0] or resp_crc[1] != expected_crc[1]:
-            raise OSError('invalid response CRC')
+    def _validate_resp_hdr(response: bytearray, slave_addr: int, length) -> bytearray:
+        resp_crc = struct.unpack("<H", response[-_CRC_LENGTH:])[0]
+        expected_crc = Serial._crc16(response[:-_CRC_LENGTH])
+        if resp_crc != expected_crc:
+            raise ValueError(f"invalid response CRC: expected {expected_crc} got {resp_crc}")
 
         if response[0] != slave_addr:
-            raise ValueError('wrong slave address')
+            raise ValueError(f"wrong slave address expected {slave_addr} got {response[0]}")
 
-        hdr_length = (_RESPONSE_HDR_LENGTH + 1) if count else _RESPONSE_HDR_LENGTH
+        if response[1] >= _ERROR_BIAS:
+            raise ValueError(f"slave returned exception code: {response[2]}")
 
-        return response[hdr_length : len(response) - _CRC_LENGTH]
+        header_length, body_length = length
+        return response[header_length : header_length + body_length]
 
     # async def read_coils(self, slave_addr: int, starting_addr: int, coil_qty: int) -> bytearray:
     #     modbus_pdu = functions.read_coils(starting_addr, coil_qty)
     #     response_length = 2 + int(ceil(coil_qty / 8))
+    #     length = (3, int(ceil(coil_qty / 8))
     #     return await self._send_receive(modbus_pdu, slave_addr, response_length, True)
 
 
     # async def read_discrete_inputs(self, slave_addr: int, starting_addr: int, input_qty: int) -> bytearray:
     #     modbus_pdu = functions.read_discrete_inputs(starting_addr, input_qty)
-    #     response_length = 2 + int(ceil(input_qty / 8))
+    #     length = (3, int(ceil(input_qty / 8))
     #     return await self._send_receive(modbus_pdu, slave_addr, response_length, True)
 
 
     # async def read_holding_registers(self, slave_addr: int, starting_addr: int, register_qty: int) -> bytearray:
     #     modbus_pdu = functions.read_holding_registers(starting_addr, register_qty)
-    #     response_length = 2 + register_qty * 2
+    #     length = (3, register_qty * 2)
     #     return await self._send_receive(modbus_pdu, slave_addr, response_length, True)
 
 
     async def read_input_registers(self, slave_addr: int, starting_address: int, register_quantity: int) -> bytearray:
         modbus_pdu = functions.read_input_registers(starting_address, register_quantity)
-        # response_length = 2 + register_quantity * 2
-        return await self._send_receive(modbus_pdu, slave_addr, True)
+        length = (3, register_quantity * 2)
+        return await self._send_receive(modbus_pdu, slave_addr, length)
 
 
     # async def write_single_coil(self, slave_addr: int, output_address: int, output_value: int):
